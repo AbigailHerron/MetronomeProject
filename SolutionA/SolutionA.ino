5a13956825809a3cc801260f3df56f763a5025f6
Abigail Herron <abigailherron@hotmail.com>
2020-03-04 01:39:59 -0800

    Updated Comments
    Provided a list of all possible changes that must be made and how to make them if MIC is DIGITAL
    
    Also corrected logical error for variable b.

--- SolutionA/SolutionA.ino
+++ SolutionA/SolutionA.ino
@@ -10,6 +10,7 @@
   but comments are placed where and how these lines should be changed if MIC is indeed Digital.
     - Will place the LED in slot 7, and MIC in slot 13 to keep them as far away from each other as possible
       on the Arduino Yun board.
+    - Included are 6 listed changes to be made on the off chance MIC is Digital
 
   4) Included a constant called THRESHOLD to hold the sound value.  If this needs tweaking, change the value
      of the constant - this will update throughout the rest of the program automatically
@@ -20,10 +21,10 @@
 // Declaring Variables and Constants
   // Declaring pins
 const int LED = 7;
-const int MIC = A0; // NOTE: if MIC is a digital input, replace A0 with 13
+const int MIC = A0; // 1) NOTE: if MIC is a digital input, replace line with: const int MIC = 13
 
   // Declaring sound threshold here
-const int THRESHOLD = 500;  // NOTE: if MIC is digital, replace line with: const string THRESHOLD = "HIGH";
+const int THRESHOLD = 500;  // 2) NOTE: if MIC is digital, replace line with: const string THRESHOLD = "HIGH";
 const unsigned long MINUTE = 60000; // there are 60 thousand milliseconds in a minute
 
   // Declaring millisecond variables
@@ -38,7 +39,7 @@ int beat = 0;
 // Setting up Pins
 void setup() {
   pinMode(LED, OUTPUT);
-  Serial.begin(9600); // NOTE: if MIC is digital, replace line with: pinMode(MIC, INPUT);
+  Serial.begin(9600); // 3) NOTE: if MIC is digital, replace line with: pinMode(MIC, INPUT);
 
 }
 
@@ -48,7 +49,7 @@ void setup() {
 // Running the Program
 void loop() {
   // declaring val variable (local to loop only)
-  val = analogRead(MIC); // NOTE: if MIC is digital, replace line with: val = digitalRead(MIC);
+  val = analogRead(MIC); // 4) NOTE: if MIC is digital, replace line with: val = digitalRead(MIC);
   GetBeats(val);
 
   digitalWrite(LED, HIGH);
@@ -61,18 +62,18 @@ void loop() {
 
 
 // CALCULATES HOW MANY BEATS THERE ARE IN A MINUTE BASED ON THE INPUT OF THE MIC
-unsigned long GetBeats(int sound) // NOTE: if MIC is digital, replace line with: int GetBeats(string sound);
+unsigned long GetBeats(int sound) // 5) NOTE: if MIC is digital, replace line with: int GetBeats(string sound);
 {
   a = millis(); // Getting initial time here so it doesn't update within the loop
   
   while((b) < 60000) // Until 6 seconds have passed
   {
-    if(sound > THRESHOLD) // NOTE: if MIC is digital, replace line with: if(sound == THRESHOLD)
+    if(sound > THRESHOLD) // 6) NOTE: if MIC is digital, replace line with: if(sound == THRESHOLD)
     {
       beat++; // increments the number of beats
     }// end if
     
-    b = a - millis(); // this will update the length of time stored in b
+    b = millis() - a; // this will update the length of time stored in b
   }// end while
 
   // updating beats to match a minutes worth
