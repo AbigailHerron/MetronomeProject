a4bf587acdffc7736574fadf2b16defe7592d99c
Abigail Herron <abigailherron@hotmail.com>
2020-03-03 17:52:06 -0800

    Update MetronomeClockTest.ino

--- MetronomeClockTest/MetronomeClockTest.ino
+++ MetronomeClockTest/MetronomeClockTest.ino
@@ -1,14 +1,14 @@
 // THIS PROGRAM IS A TEST - TRYING TO GET THE TIMING INTERVAL BETWEEN TWO SOUNDS USING CLOCK()
-
-// inport the right libraries
-#include <time.h> // something to do with time - importing the clock_t() function
+#include <time.h>
 
 // declare constants for the pin locations
 const int LED = 7; // digital output pin 7
 const int MIC = A0; // analogue input pin A0
 
 
-
+// declaring variables
+int beatCount = 0;
+ clock_t t;
 
 void setup() {
   Serial.begin(9600); // sets the pin mode for the analog MIC 
@@ -16,11 +16,6 @@ void setup() {
 }
 
 void loop() {
-
-  // creating local variables
-  int beatCount = 0;
-  clock_t t;
-
 
   // finding the time interval between two beats
 //---------------------------------------------------------------------
