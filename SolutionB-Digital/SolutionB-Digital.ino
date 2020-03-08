93f6e939054429d536cfa16897448a99e4cd55d2
Abigail Herron <abigailherron@hotmail.com>
2020-03-08 16:07:02 -0700

    Update SolutionB-Digital.ino

--- SolutionB-Digital/SolutionB-Digital.ino
+++ SolutionB-Digital/SolutionB-Digital.ino
@@ -9,7 +9,7 @@ const int LED = 7; // Place LED in pin 7 please
 const int MIC = 9; // Place MIC sensor in pin 9 please
 
   // Declaring sound threshold here
-const int THRESHOLD = 0;
+const int THRESHOLD = 200;
 
 
   // Declaring millisecond variables
