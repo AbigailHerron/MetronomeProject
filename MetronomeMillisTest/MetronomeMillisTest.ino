34fd75535d7a0048b5db71671e64b233b55dbbdf
Abigail Herron <abigailherron@hotmail.com>
2020-03-07 17:23:37 -0800

    Update MetronomeMillisTest.ino

--- MetronomeMillisTest/MetronomeMillisTest.ino
+++ MetronomeMillisTest/MetronomeMillisTest.ino
@@ -1,36 +1,48 @@
 // THIS PROGRAM IS A TEST - TRYING TO GET THE TIMING INTERVAL BETWEEN TWO SOUNDS USING MILLIS()
+/*After some research on how C++ works (bare in mind, I have no C++ knowledge), I have updated this test so
+  that it actually works.
+  
+  CHANGES MADE: 
+  1)  Removed the MIC sensor (just need to know if millis() works)
+              -  Also removed all assosiated variables
+              
+  2)  Removed Serial.begin(9600) - I'll be honest, even after researching this, I still don't know what it does.
+      I think it deals with how many bits of data are processed per second - in relation to the console, as in
+      how quickly information is printed to the screen.
+      
+  3)  Moved the time-recording section to a new function - GetDelay()*/
 
 // declare constants for the pin locations
 const int LED = 7; // digital output pin 7
-const int MIC = A0; // analogue input pin A0
 
 
 // declaring variables
-int beatCount = 0;
-unsigned long t;
+unsigned long t1; // takes first time-stamp using millis()
+unsigned long t2; // records the differents between the first time-stamp (t1) and the current millis()
+
+
 
 void setup() {
-  Serial.begin(9600); // sets the pin mode for the analog MIC 
   pinMode(LED, OUTPUT); // this is making the LED an output devide in the program
+  GetDelay(); // setting up the timing delay here since the function only needs to run once
 }
 
-void loop() {
 
-  // finding the time interval between two beats
-//---------------------------------------------------------------------
-  while(beatCount != 2)
-  {
-    if(analogRead(MIC) > 500) // the 500 may have to be raised or lowered depending on how noisy the room is
-    {
-      t = millis(); // records the time-stamp the first sound is made
-      beatCount++; // increases the beatCount if a successful sound is recorded
-    }
-  }
-  t = millis() - t; // takes the previous time-stamp away from the current time-stamp
- //---------------------------------------------------------------------
-  
+
+void loop() {
   digitalWrite(LED, HIGH);
-  delay(250);
+  delay(100);
   digitalWrite(LED, LOW);
-  delay(t - 250);
+  delay(t2);
+}
+
+
+
+// GetDelay() assigns the delay to variable t2
+unsigned long GetDelay()
+{
+  t1 = millis();
+  delay(350); // I'm not sure how long this is, I know it's more than a 1/4 second and less than 1/2 a second
+  t2 = t1 - millis();
+  return t2;
 }
