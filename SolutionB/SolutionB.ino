24f01ef86425a67953fc826b941d9ae79eca1f19
Abigail Herron <abigailherron@hotmail.com>
2020-03-08 15:54:24 -0700

    Update SolutionB.ino

--- SolutionB/SolutionB.ino
+++ SolutionB/SolutionB.ino
@@ -78,7 +78,7 @@ void loop() {
 
   // LED begins blinking to tempo
   digitalWrite(LED, HIGH);
-  delay(250); // LED shoudl be on for a quater of a second
+  delay(100);
   digitalWrite(LED, LOW);
   delay(c);
 }// end loop()
