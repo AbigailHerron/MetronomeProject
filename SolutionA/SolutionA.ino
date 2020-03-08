851ebc44b8f38a4e6db457790df23819b9152ee4
Abigail Herron <abigailherron@hotmail.com>
2020-03-07 16:58:13 -0800

    Update SolutionA.ino

--- SolutionA/SolutionA.ino
+++ SolutionA/SolutionA.ino
@@ -1,4 +1,4 @@
-@m// THIS IS A POSSIBLE SOLUTION TO METRONOME PROBLEM - THIS SHOULD RUN
+// THIS IS A POSSIBLE SOLUTION TO METRONOME PROBLEM - THIS SHOULD RUN
 /*NOTES:
  * 1) Continuing forward with millis() as the measurement used is in milliseconds, which is consistent
   with the delay() fuction, also measured in milliseconds.
@@ -6,25 +6,21 @@
   2) Clock() revolves around clocks per cycle which makes it incompatible with delay() when used raw - would
   have to incorperate CLOCKS_PER_SEC, thus making the program more complicated
 
-  3) Was unsure if the MIC input pin was going to be Analogue or Digital.  For now, it is set to Analogue,
-  but comments are placed where and how these lines should be changed if MIC is indeed Digital.
-    - Will place the LED in slot 7, and MIC in slot 13 to keep them as far away from each other as possible
-      on the Arduino Yun board.
-    - Included are 7 listed changes to be made on the off chance MIC is Digital
+  3) Was unsure if the MIC input pin was going to be Analogue or Digital.  For now, it is set to Analogue
 
   4) Included a constant called THRESHOLD to hold the sound value.  If this needs tweaking, change the value
      of the constant - this will update throughout the rest of the program automatically
   
   5) Creating a function to calculate beats per minute as this gives a better METRONOME rhythm than the
-  random length of time between two beats, and thus better for actually keeping a musical beat.*/
+  random length of time between two beats, and thus better for actually keeping a musical beat*/
 
 // Declaring Variables and Constants
   // Declaring pins
-const int LED = 7;
-const int MIC = A0; // 1) NOTE: if MIC is a digital input, replace line with: const int MIC = 13
+const int LED = 7;  // Place LED in pin 7 please
+const int MIC = A0; // Place MIC in pin A0 please
 
   // Declaring sound threshold here
-const int THRESHOLD = 500;  // 2) NOTE: if MIC is digital, replace line with: const string THRESHOLD = "HIGH";
+const int THRESHOLD = 0; // feel free to raise this value if MIC is picking up background noise
 const unsigned long MINUTE = 60000; // there are 60 thousand milliseconds in a minute
 
   // Declaring millisecond variables
@@ -38,9 +34,9 @@ int beat = 0;
 
 // Setting up Pins
 void setup() {
+  pinMode(MIC, INPUT);
   pinMode(LED, OUTPUT);
-  Serial.begin(9600); // 3) NOTE: if MIC is digital, replace line with: pinMode(MIC, INPUT);
-
+  Serial.begin(9600);  // Unsure whether this is necessary or not...
 }
 
 
@@ -53,7 +49,7 @@ void loop() {
   if(beat == 0)
   {
     // declaring val variable (local to loop only)
-    unsigned long val = analogRead(MIC); // 4) NOTE: if MIC is digital, replace line with: unsigned long val = digitalRead(MIC);
+    unsigned long val = analogRead(MIC);
     unsigned long beatDelay = GetBeats(val);
   }
 
@@ -67,18 +63,18 @@ void loop() {
 
 
 // CALCULATES HOW MANY BEATS THERE ARE IN A MINUTE BASED ON THE INPUT OF THE MIC
-unsigned long GetBeats(unsigned long sound) // 5) NOTE: if MIC is digital, replace line with: int GetBeats(string sound);
+unsigned long GetBeats(unsigned long sound)
 {
   a = millis(); // Getting initial time here so it doesn't update within the loop
   
   while(b < 6000) // Until 6 seconds have passed
   {
-    if(sound > THRESHOLD) // 6) NOTE: if MIC is digital, replace line with: if(sound == THRESHOLD)
+    if(sound > THRESHOLD)
     {
       beat++; // increments the number of beats
     }// end if
 
-    sound = analogRead(MIC); // 7) NOTE: if MIC is digital, replace line with: sound = digitalRead(MIC);
+    sound = analogRead(MIC);
     b = millis() - a; // this will update the length of time stored in b
   }// end while
 
